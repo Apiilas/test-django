2ff5cbca57d864ffb9437fcb2a9f834be54f9434
jordan <jordan@MacBook-Pro-de-jordan-2.local>
2023-02-03 23:20:20 +0100

    feat(mytest): add templates success and success action

--- mytest/home/urls.py
+++ mytest/home/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 from .views import BookList
 
+
 urlpatterns = [
     path('index', views.index, name='index'),
     path('buy-book', BookList.as_view(), name='buy_book'),
--- mytest/home/views.py
+++ mytest/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from django.views.generic import ListView
 from .models import Book
 
@@ -9,5 +9,14 @@ def index(request):
 
 class BookList(ListView):
     model = Book
-    template_name = 'templates/book_list.html'
+    template_name = 'book_list.html'
     queryset = Book.objects.all()
+
+    def post(self, request):
+        if request.method == 'POST':
+            book_id = request.POST.get('book_id')
+            book = Book.objects.get(id=book_id)
+
+            return render(request, 'success_buy.html', {'book': book})
+        else:
+            return redirect('book_list.html')
